945f9c7bd5b215b3cb2307f2ef17722fa242ad43
luankz <17682333171@163.com>
2024-05-02 17:53:00 +0800

    模型详情，大致布局

--- src/views/model/info/index.vue
+++ src/views/model/info/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="app-container">
-    <h3>模型列表</h3>
+  <div class="container-app">
+    <div class="header">
+      <router-link to="/">
+        <div class="back-button">
+          <span>返回</span>
+          <span>模型详情</span>
+        </div>
+      </router-link>
+    </div>
+    <div class="model-info">
+      <!---->
+    </div>
+    <div class="model-tabs">
+      <!-- Your page content here -->
+    </div>
+    <div class="model-content">
+      <!-- Your page content here -->
+    </div>
   </div>
 </template>
