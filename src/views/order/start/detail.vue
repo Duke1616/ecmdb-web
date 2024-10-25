8538fe637e4d40567c7f554af5d548dbf844a9a8
luankz <17682333171@163.com>
2024-10-25 09:17:21 +0800

    chore: 动态植入 form-data 用户信息

--- src/views/order/start/detail.vue
+++ src/views/order/start/detail.vue
@@ -6,13 +6,14 @@
 
 <script setup lang="ts">
 import { detailTemplateApi } from "@/api/template"
-import formCreate from "@form-create/element-ui"
+import formCreate, { Rule } from "@form-create/element-ui"
 import { ref } from "vue"
 import { cloneDeep } from "lodash-es"
 import { ElMessage } from "element-plus"
 import { createOrderApi } from "@/api/order"
 import { createOrderReq } from "@/api/order/types/order"
 import { template } from "@/api/template/types/template"
+import { useUserStore } from "@/store/modules/user"
 
 const emits = defineEmits(["closed"])
 const onClosed = () => {
@@ -32,7 +33,7 @@ const DEFAULT_FORM_DATA: createOrderReq = {
 const formData = ref<createOrderReq>(cloneDeep(DEFAULT_FORM_DATA))
 
 const data = ref()
-const rule = ref()
+const rule = ref<Rule[]>()
 const options = ref({
   //表单提交事件
   onSubmit: function () {}
@@ -46,12 +47,53 @@ const handleDetail = (id: number) => {
       options.value = { ...resData.options }
       rule.value = resData.rules
 
+      if (rule.value !== undefined) {
+        processRules(rule.value)
+      }
+      // API 请求插入调用参数
+
+      console.log(options.value)
+      console.log(rule.value)
+
       // 将 onSubmit 函数绑定到闭包函数
       options.value.onSubmit = handleSubmit(resData)
     })
     .catch((error) => {
       console.log("catch", error)
     })
+}
+function processRules(rules: Rule[]) {
+  rules.forEach(async (rule: Rule) => {
+    if (typeof rule.effect?.fetch === "object" && "data" in rule.effect.fetch) {
+      let fetchData: { [key: string]: any } = {}
+
+      if (typeof rule.effect.fetch.data === "string") {
+        try {
+          fetchData = JSON.parse(rule.effect.fetch.data)
+        } catch (e) {
+          console.error("Failed to parse fetch.data as JSON:", e)
+          fetchData = {}
+        }
+      } else if (typeof rule.effect.fetch.data === "object" && rule.effect.fetch.data !== null) {
+        fetchData = rule.effect.fetch.data
+      }
+      await useUserStore().getInfo()
+      fetchData.username = useUserStore().username
+
+      // 重新赋值给 fetch.data
+      rule.effect.fetch.data = fetchData
+    }
+
+    // 递归处理 control 字段
+    if (rule.control) {
+      processRules(rule.control)
+    }
+
+    // 递归处理 rule 字段
+    if (rule.rule) {
+      processRules(rule.rule)
+    }
+  })
 }
 
 const handleSubmit = (resData: template) => {
