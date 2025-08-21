8c8c1bb4b71a073c324ed93dc15c9248c09ec335
luankz <17682333171@163.com>
2025-08-21 16:56:08 +0800

    chore: 升级本地 node 版本，兼容性处理

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import router from "@/router"
 import "@/router/permission"
 // load
 import { loadSvg } from "@/icons"
-import { loadPlugins } from "@/plugins"
+import { installPlugins } from "@/plugins"
 import { loadDirectives } from "@/directives"
 // css
 import "uno.css"
@@ -17,21 +17,22 @@ import "element-plus/theme-chalk/dark/css-vars.css"
 import "vxe-table/lib/style.css"
 import "vxe-table-plugin-element/dist/style.css"
 import "@/styles/index.scss"
-import { loadCodeMirror } from "./plugins/codemirror"
 // import "@smallwei/avue/lib/index.css"
 
 const app = createApp(App)
 
-/** 加载插件 */
-loadPlugins(app)
+// 安装插件（全局组件、自定义指令等）
+installPlugins(app)
+
 /** 加载全局 SVG */
 loadSvg(app)
 /** 加载自定义指令 */
 loadDirectives(app)
-/** 加载vue-codemirror */
-loadCodeMirror(app)
 
+// 安装 pinia 和 router
 app.use(store).use(router)
+
+// router 准备就绪后挂载应用
 router.isReady().then(() => {
   app.mount("#app")
 })
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,12 +5,14 @@ import { loadVxeTable } from "./vxe-table"
 import { loadFromCreate } from "./form-create"
 import { loadIconPicker } from "./icon-picker"
 import { loadFinder } from "./finder"
+import { loadCodeMirror } from "./codemirror"
 
-export function loadPlugins(app: App) {
+export function installPlugins(app: App) {
   loadElementPlus(app)
   loadElementPlusIcon(app)
   loadFromCreate(app)
   loadVxeTable(app)
   loadIconPicker(app)
   loadFinder(app)
+  loadCodeMirror(app)
 }
