a4f3b2ff4865301901c47c82abb7f72f1590536a
luankz <17682333171@163.com>
2025-09-13 14:43:05 +0800

    chore: 简化流程控制里面的样式

--- src/common/components/workflow/PropertySetting/index.ts
+++ src/common/components/workflow/PropertySetting/index.ts
@@ -1,3 +1,2 @@
-export { default as PropertyContainer } from './PropertyContainer.vue'
-export { default as FormSection } from './FormSection.vue'
-export { default as FormHelp } from './FormHelp.vue'
+export { default as FormSection } from "./FormSection.vue"
+export { default as FormHelp } from "./FormHelp.vue"
