ac9e3d1153cd73afea1c0626edc2f07f0ee1ebfc
duke1616 <17682333171@163.com>
2026-06-12 20:36:59 +0800

    chore: 导入 svg 图片

--- src/plugins/icon-svg/index.ts
+++ src/plugins/icon-svg/index.ts
@@ -1,8 +1,8 @@
 import type { App } from "vue"
 import SvgIcon from "~virtual/svg-component"
 
-// 使用 Vite 的 import.meta.glob 批量静态引入 icons 目录下所有 SVG 文件，防止生产打包时 Tree Shaking 导致动态加载失败
-import.meta.glob("@@/assets/icons/**/*.svg")
+// 使用 Vite 的 import.meta.glob 批量静态引入 icons 目录下所有 SVG 文件，使用 eager: true 强制同步打入，防止生产打包时 Tree Shaking 导致动态加载失败
+import.meta.glob("@@/assets/icons/**/*.svg", { eager: true })
 
 export function loadIconSvg(app: App) {
   // 注册 SvgIcon 组件
