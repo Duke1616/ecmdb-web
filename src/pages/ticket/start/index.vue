09e0441b3a8a6b3a9952a3eb2792907b9530a1d3
duke1616 <17682333171@163.com>
2026-07-02 18:19:23 +0800

    chore: 工单收藏功能

--- src/pages/ticket/start/index.vue
+++ src/pages/ticket/start/index.vue
@@ -101,7 +101,7 @@ import type { template } from "@/api/ticket/template/types/template"
 const { hasPermission } = usePermission()
 
 const canCreateTicket = computed(() => hasPermission(TICKET_CAPABILITIES.Manager.Submit))
-const canFavoriteTemplate = computed(() => hasPermission(TICKET_CAPABILITIES.Template.View))
+const canFavoriteTemplate = computed(() => hasPermission(TICKET_CAPABILITIES.Manager.Submit))
 
 const { selectedCategory, searchQuery } = useTemplateFilter()
 
