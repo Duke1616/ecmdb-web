6804ec70e302fb16e4088a27532d5831920a72ec
duke1616 <17682333171@163.com>
2026-09-09 20:43:41 +0800

    chore(proxy): 精简 Vite 与 Nginx 代理规则，统一通过 /api/iam 转发

--- vite.config.ts
+++ vite.config.ts
@@ -179,10 +179,6 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api\/iam/, "/api")
         },
-        "/saml": {
-          target: `http://${viteEnv.VITE_IAM_API}`,
-          changeOrigin: true
-        },
         "/minio": {
           target: `http://${viteEnv.VITE_MINIO_ENDPOINT}`,
           changeOrigin: true,
